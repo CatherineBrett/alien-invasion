908c473976d7cfc5e09856ad6052a116ed9ad6d5
Catherine Brett <catherinejanebrett@icloud.com>
2024-10-22 11:48:06 +0100

    Draws the ship to the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,8 +1,7 @@
 import sys
-
 import pygame
-
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -22,6 +21,8 @@ class AlienInvasion:
         # Set the background colour.
         self.bg_colour = (230, 230, 230)
 
+        self.ship = Ship(self)
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -32,6 +33,7 @@ class AlienInvasion:
 
             # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_colour)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
